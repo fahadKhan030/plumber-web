2000601bbff8148f8646a399e336f5de8eeaa10b
fahadkhan030 <fahdkhn30@gmail.com>
2025-07-13 22:35:51 +0500

    Update Header component styles and add Button component with contact icon

---
+++ src/component/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({ text }) => {
+  return (
+    <div className="bg-[#F3E063] px-3 py-2 rounded-md text-black font-semibold">
+      {text}
+    </div>
+  );
+};
+
+export default Button;
--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,22 +1,28 @@
 import React from "react";
-import Logo from "../assets/Logo.png";
+import Logo from "../assets/Logo.avif";
+import Button from "../component/Button.jsx";
+import telephone from "../assets/tele-Icon.png";
+
 export const Header = () => {
   return (
-    <div className="flex  justify-between max-w-[1200px] bg-gray-800 text-white py-4 text-center">
+    <div className="flex bg-[#1D65D1] items-center justify-between max-w-[1200px] text-white py-4 text-center px-2">
       <div>
-        <img src={Logo} alt="Logo" className="h-5" />
+        <img src={Logo} alt="" className="h-6" />
       </div>
 
-      <ul className="flex justify-center gap-4">
+      <ul className="flex justify-center px-3 py-2 text-white backdrop-blur-sm bg-white/10 rounded-md font- gap-4">
         <li>Home</li>
         <li>About</li>
         <li>Services</li>
         <li>Contact</li>
       </ul>
 
-      <div className="flex itmems-center gap-4">
-        <button>Menu</button>
-        <button>Contact</button>
+      <div className="flex items-center gap-4">
+        <button className="flex items-center justify-center gap-1">
+          <img src={telephone} alt="" className="h-4" />
+          <span>(333) 8958158</span>
+        </button>
+        <Button text="Contact" />
       </div>
     </div>
   );
