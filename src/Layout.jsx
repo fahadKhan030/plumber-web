d7031d7b6deea20d444ada80ac57a17b33b873bf
fahadkhan030 <fahdkhn30@gmail.com>
2025-08-10 23:37:57 +0500

    Add Footer component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Header } from "./header/Header.jsx";
-import Footer from "./footer/Footer.jsx";
+import Footer from "./Footer/Footer.jsx";
 
 const Layout = () => {
   return (
