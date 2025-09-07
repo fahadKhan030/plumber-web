eeb4c4db8d1dc31db1424ff9134a81ffc8d645c1
fahadkhan030 <fahdkhn30@gmail.com>
2025-09-07 22:04:17 +0500

    Refactor Services component to use GSAP for animations and streamline serviceCard definition

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -5,58 +5,72 @@ import plumbing from "../assets/plumbingb.png";
 import tub from "../assets/tubb.png";
 import wasterwater from "../assets/drainb.png";
 import driver from "../assets/driverb.png";
-import right from "../assets/arrowright.png";
 import "../App.css";
 import Button from "../component/Button";
 
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const serviceCard = [
+  {
+    id: 1,
+    title: "Leak Detection",
+    description:
+      "24/7 emergency plumbing services to handle any urgent issues.",
+    image: drop,
+  },
+  {
+    id: 2,
+    title: "Drain Cleaning",
+    description: "Advanced leak detection services to prevent water damage.",
+    image: wasterwater,
+  },
+  {
+    id: 3,
+    title: "Water Heater Installation",
+    description:
+      "Professional drain cleaning services to keep your pipes clear.",
+    image: driver,
+  },
+  {
+    id: 4,
+    title: "Bathroom & Kitchen Plumbing",
+    description:
+      "Professional drain cleaning services to keep your pipes clear.",
+    image: tub,
+  },
+  {
+    id: 5,
+    title: "Water Filtration Systems",
+    description:
+      "Professional drain cleaning services to keep your pipes clear.",
+    image: fan,
+  },
+  {
+    id: 6,
+    title: "Pipe Repair & Maintenance",
+    description: "Professional pipe repair services to fix leaks and damages.",
+    image: plumbing,
+  },
+];
+
 const Services = () => {
-  const serviceCard = [
-    {
-      id: 1,
-      title: "Leak Detection",
-      description:
-        "24/7 emergency plumbing services to handle any urgent issues.",
-      image: drop, // Replace with actual image path
-    },
-    {
-      id: 2,
-      title: "Drain Cleaning",
-      description: "Advanced leak detection services to prevent water damage.",
-      image: wasterwater,
-    },
-    {
-      id: 3,
-      title: "Water Heater Installation",
-      description:
-        "Professional drain cleaning services to keep your pipes clear.",
-      image: driver,
-    },
-    {
-      id: 4,
-      title: "Bathroom & Kitchen Plumbing",
-      description:
-        "Professional drain cleaning services to keep your pipes clear.",
-      image: tub,
-    },
-    {
-      id: 5,
-      title: "Water Filtration Systems",
-      description:
-        "Professional drain cleaning services to keep your pipes clear.",
-      image: fan,
-    },
-    {
-      id: 6,
-      title: "Pipe Repair & Maintenance",
-      description:
-        "Professional pipe repair services to fix leaks and damages.",
-      image: plumbing,
-    },
-  ];
+  useGSAP(() => {
+    gsap.to(".serviesCard", {
+      y: 0, // start further down for smooth motion
+      opacity: 1,
+      duration: 1.2,
+      stagger: 0.25,
+      // ease: "back.out(1.7)", // smoother bounce-like easing
+    });
+  }, []);
 
   return (
     <div>
-      <div className="relative text-white gradient-bg  h-[35vh] lg:h-[45vh] pt-20 lg:pt-36 w-full">
+      <div className="relative text-white gradient-bg h-[35vh] lg:h-[45vh] pt-20 lg:pt-36 w-full">
         <div className="flex items-center justify-center flex-col max-w-[1200px] mx-auto">
           <h1 className="text-3xl z-40 md:text-5xl lg:text-6xl font-semibold">
             Services
@@ -68,14 +82,14 @@ const Services = () => {
         </div>
       </div>
 
-      <div className="w-full  px-4 py-20 mt-10">
-        <div className="grid grid-cols- 1 md:grid-cols-2 lg:grid-cols-3 gap-1 max-w-[1200px] mx-auto ">
-          {serviceCard.map((service, index) => (
+      <div className="w-full px-4 py-20 mt-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 max-w-[1200px] mx-auto">
+          {serviceCard.map((service) => (
             <div
-              key={service.index}
-              className="flex flex-col md:flex-row  gap-7 max-w-[1200px] mx-auto mt-10"
+              key={service.id}
+              className="serviesCard transform translate-y-11 opacity-0 mt-10"
             >
-              <div className="flex flex-col gap-3 md:gap-10 md: hover:scale-95 transform-all duration-300 hover:cursor-pointer text-start border-[0.1px] shadow-xl bg-[#e6eefa] border-gray-400  px-5 py-7 rounded-xl ">
+              <div className=" flex flex-col gap-3 md:gap-10 hover:scale-95 transition-transform duration-300 hover:cursor-pointer text-start border-[0.1px] shadow-xl bg-[#e6eefa] border-gray-400 px-5 py-7 rounded-xl">
                 <img
                   src={service.image}
                   alt={service.title}
